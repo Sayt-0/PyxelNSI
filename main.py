18a7527223d95ee309c550082e9f86f082fafc8a
unknown <dalmorolouis@gmail.com>
2024-09-24 14:58:13 +0200

    debut joueur

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
 import pyxel 
 
+
+
+
+class Joueur1:
+    def __init__(self):
+    self.x=20
+    self.y=pyxel.height/2
+    self.width=10
+    self.height=30
+    self.vitesse=4
+
+
+
+def update(self):
+    if pyxel.btn(pyxel.KEY_A) == True:
+        self.y=self.y-self.vitesse
+    elif pyxel.btn(pyxel.key_Q) == True:
+        self.y=self.y+self.vitesse
+
+
+def draw(self):
+    pyxel.rect(self.x, self.y, self.width, self.height, 7)
+
 class Game:
     def __init__(self) -> None:
-        pyxel.init(160, 120)
+        pyxel.init(160, 120, 'Bougnoule', 120)
         pyxel.run(self.update, self.draw)
     
     def update(self):
@@ -13,4 +36,3 @@ class Game:
 Game()
 
 
-##dsdsdsdsdsdsdsds
